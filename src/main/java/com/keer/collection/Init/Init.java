31f1f3f11ffddf5b46b2db8e8effabf10b7a7e64
keer123456789 <15110074528@163.com>
2019-04-12 14:39:23 +0800

    add new function

--- src/main/java/com/keer/collection/Init/Init.java
+++ src/main/java/com/keer/collection/Init/Init.java
@@ -1,13 +1,19 @@
 package com.keer.collection.Init;
 
 import com.keer.collection.Util.HttpUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 
 public class Init implements CommandLineRunner {
+
+    protected static Logger logger= LoggerFactory.getLogger(Init.class);
+
     @Override
     public void run(String... args) throws Exception {
         for (; true; ) {
             if (HttpUtil.httpGet("192.168.137.1:8080/setRaspberryIP").equals("true")) {
+                logger.info("注册成功");
                 break;
             }
         }
