f32bcfb8c5de4b712f1a804ece1be8d4f335b7b5
keer123456789 <15110074528@163.com>
2019-04-13 15:30:05 +0800

    add @@EnableScheduling

--- src/main/java/com/keer/collection/CollectionApplication.java
+++ src/main/java/com/keer/collection/CollectionApplication.java
@@ -6,7 +6,9 @@ import org.glassfish.tyrus.core.wsadl.model.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CollectionApplication {
 
