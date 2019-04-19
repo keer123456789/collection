d93c76c25badc51d30e5da876fb886a2f43fd4e3
keer123456789 <15110074528@163.com>
2019-04-19 16:02:32 +0800

    fix new send data

--- src/main/java/com/keer/collection/Service/CollectionService.java
+++ src/main/java/com/keer/collection/Service/CollectionService.java
@@ -30,7 +30,6 @@ public class CollectionService {
     @Autowired
     BigchainDBUtil bigchainDBUtil;
 
-    private boolean isUse=false;
 
     /**
      * 获得env.json的文件中的数据
@@ -55,77 +54,73 @@ public class CollectionService {
      * @return
      */
     public boolean sendData(List<Map> data, String assetId) throws InterruptedException {
-        if(!isUse) {
-            isUse=true;
 
 
-            double tem = 0.0;
-            int temp = 0;
-            double hum = 0.0;
-            int humi = 0;
-            double co2 = 0.0;
-            int co2sum = 0;
-            for (Map map : data) {
-                if (!map.get("temperture").toString().equals("fail")) {
-                    tem = tem + Double.valueOf(map.get("temperture").toString());
-                    temp++;
-                }
-                if (!map.get("humidity").toString().equals("fail")) {
-                    hum = hum + Double.valueOf(map.get("humidity").toString());
-                    humi++;
-                }
-                if (!map.get("CO2").toString().equals("fail")) {
-                    co2 = co2 + Double.valueOf(map.get("CO2").toString());
-                    co2sum++;
-                }
+        double tem = 0.0;
+        int temp = 0;
+        double hum = 0.0;
+        int humi = 0;
+        double co2 = 0.0;
+        int co2sum = 0;
+        for (Map map : data) {
+            if (!map.get("temperture").toString().equals("fail")) {
+                tem = tem + Double.valueOf(map.get("temperture").toString());
+                temp++;
             }
-            Map map = new HashMap();
-
-            //判断正常温度的个数，做平均数
-            if (temp == 0) {
-                map.put("temperture", "fail");
-            } else {
-                tem = tem / temp;
-                map.put("temperture", tem + "");
+            if (!map.get("humidity").toString().equals("fail")) {
+                hum = hum + Double.valueOf(map.get("humidity").toString());
+                humi++;
             }
-
-            //判断正湿度度的个数，做平均数
-            if (humi == 0) {
-                map.put("humidity", "fail");
-            } else {
-                hum = hum / humi;
-                map.put("humidity", hum + "");
+            if (!map.get("CO2").toString().equals("fail")) {
+                co2 = co2 + Double.valueOf(map.get("CO2").toString());
+                co2sum++;
             }
+        }
+        Map map = new HashMap();
 
-            //判断正常二氧化碳的个数，做平均数
-            if (co2sum == 0) {
-                map.put("CO2", "fail");
-            } else {
-                co2 = co2 / co2sum;
-                map.put("CO2", co2 + "");
-            }
+        //判断正常温度的个数，做平均数
+        if (temp == 0) {
+            map.put("temperture", "fail");
+        } else {
+            tem = tem / temp;
+            map.put("temperture", tem + "");
+        }
+
+        //判断正湿度度的个数，做平均数
+        if (humi == 0) {
+            map.put("humidity", "fail");
+        } else {
+            hum = hum / humi;
+            map.put("humidity", hum + "");
+        }
+
+        //判断正常二氧化碳的个数，做平均数
+        if (co2sum == 0) {
+            map.put("CO2", "fail");
+        } else {
+            co2 = co2 / co2sum;
+            map.put("CO2", co2 + "");
+        }
 
 
-            map.put("id", data.get(0).get("id").toString());
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String time = df.format(new Date());
-            map.put("time", time);
-            logger.info("此次最终环境数据为：" + map.toString());
+        map.put("id", data.get(0).get("id").toString());
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = df.format(new Date());
+        map.put("time", time);
+        logger.info("此次最终环境数据为：" + map.toString());
 
-            BigchainDBData bigchainDBData = new BigchainDBData("Environment", map);
-            String TXID = bigchainDBUtil.transferToSelf(bigchainDBData, assetId);
-            Thread.sleep(2000);
-            if (bigchainDBUtil.checkTransactionExit(TXID)) {
-                logger.info("交易ID：" + TXID);
-                isUse=false;
-                return true;
-            } else {
-                isUse=false;
-                return false;
-            }
-        }else{
+        BigchainDBData bigchainDBData = new BigchainDBData("Environment", map);
+        String TXID = bigchainDBUtil.transferToSelf(bigchainDBData, assetId);
+        Thread.sleep(2000);
+        if (bigchainDBUtil.checkTransactionExit(TXID)) {
+            logger.info("交易ID：" + TXID);
+
+            return true;
+        } else {
+
             return false;
         }
+
 
     }
 }
